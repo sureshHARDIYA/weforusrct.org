c5ca2a2330060739265e618b0029fa842dcee3fb
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2020-04-30 14:23:04 +0200

    demo changes for everyone

---
+++ src/data/global/blogs/newblog.md
@@ -0,0 +1,31 @@
+---
+title: "this is my new title"
+date: "2020-01-25 05:01:00"
+author: "Owen Christ"
+featured_image: "../../images/blog/blog-02-1170x600.jpg"
+categories:
+  - success story
+  - tips
+tags:
+  - business
+  - IT Security
+  - IT Services
+format: "image"
+is_featured: true
+---
+
+# This is my new heading ...
+
+## This is my 2nd level heading
+
+### This is my 3rd level heading ..
+
+#### This is my 4th level heading ...
+
+##### This 5th level
+
+This is my normal text. And this is my _italics._
+
+**This is my bold text.**
+
+> This is my quoted text.
--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,36 +1,35 @@
-import React from "react"
-import SEO from "../components/seo"
-import Layout from '../containers/layout/layout'
-import Header from '../containers/layout/header/header-one'
-import Footer from '../containers/layout/footer/footer-one'
-import PageHeader from '../containers/about-us/page-header'
-import AboutArea from '../containers/about-us/about-area'
-import ServicesArea from '../containers/about-us/services-area'
-import ResourcesArea from '../containers/about-us/resources-area'
-import SolutionsArea from '../containers/about-us/solutions-area'
-import FunFactArea from '../containers/global/funfact-area/section-one'
-import TestimonialArea from '../containers/global/testimonial-area/section-one'
-import ClientsArea from '../containers/global/clients-area/section-one'
-import ContactArea from '../containers/global/contact-area/contact-two'
+import React from "react";
+import SEO from "../components/seo";
+import Layout from "../containers/layout/layout";
+import Header from "../containers/layout/header/header-one";
+import Footer from "../containers/layout/footer/footer-one";
+import PageHeader from "../containers/about-us/page-header";
+import AboutArea from "../containers/about-us/about-area";
+import ServicesArea from "../containers/about-us/services-area";
+import ResourcesArea from "../containers/about-us/resources-area";
+import SolutionsArea from "../containers/about-us/solutions-area";
+import FunFactArea from "../containers/global/funfact-area/section-one";
+import TestimonialArea from "../containers/global/testimonial-area/section-one";
+import ClientsArea from "../containers/global/clients-area/section-one";
+import ContactArea from "../containers/global/contact-area/contact-two";
 
-const AboutPage = ({location}) => (
+const AboutPage = ({ location }) => (
   <Layout location={location}>
     <SEO title="About Us" />
-    <Header/>
+    <Header />
     <main className="site-wrapper-reveal">
-        <PageHeader/>
-        <AboutArea/>
-        <ServicesArea/>
-        <ResourcesArea/>
-        <SolutionsArea/>
-        <FunFactArea/>
-        <TestimonialArea/>
-        <ClientsArea/>
-        <ContactArea/>
+      <PageHeader />
+      <AboutArea />
+      <ServicesArea />
+      <ResourcesArea />
+      <SolutionsArea />
+      <FunFactArea />
+      <TestimonialArea />
+      <ClientsArea />
+      <ContactArea />
     </main>
-    <Footer/>
+    <Footer />
   </Layout>
-)
- 
-export default AboutPage
- 
+);
+
+export default AboutPage;
